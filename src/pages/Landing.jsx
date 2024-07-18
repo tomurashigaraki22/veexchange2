44b789ac7d77f8fefc13158aa51d49f99459a932
tomurashigaraki22 <emmanuelhudson355@gmail.com>
2024-07-18 16:57:31 +0100

    wadwd

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import img from '../assets/bghud.jpg';
 import img2 from '../assets/purplehd.jpg'
 import p1 from '../assets/rachel.jpeg'
@@ -9,10 +9,34 @@ import p5 from '../assets/haleemah.jpeg'
 import p6 from '../assets/convenant.jpeg'
 import logo from '../assets/money.png'; // Adjust the path if necessary
 import ssr from '../assets/ssr.png';
-import { Banknote, Bitcoin, Coins, CoinsIcon, CreditCard, FastForward, FileLock, LockIcon, Phone, Settings, User2, Wifi } from "lucide-react";
+import { Banknote, Bitcoin, Coins, CoinsIcon, CreditCard, FastForward, FileLock, LockIcon, MinusCircle, Phone, PlusCircle, Settings, User2, Wifi } from "lucide-react";
 import Navbar from "../components/Navbar";
 
+
+
 const Landing = () => {
+    const [openResource, setopenResource] = useState(false)
+    const [openCompany, setopenCompany] = useState(false)
+    const [openConnect, setopenConnect] = useState(false)
+    const [openGift, setopenGift] = useState(false)
+    const [openProducts, setopenProducts] = useState(true)
+
+    const openr = () => {
+        setopenResource(!openResource)
+    }
+    const opency = () => {
+        setopenCompany(!openCompany)
+    }
+    const openct = () => {
+        setopenConnect(!openConnect)
+    }
+    const opengt = () => {
+        setopenGift(!openGift)
+    }
+    const openPs = () => {
+        setopenProducts(!openProducts)
+    }
+
     return (
         <div className="w-full max-w-screen-full bg-black text-white min-h-screen bg-cover bg-center p-1 bg-white">
             {/* Navigation */}
@@ -309,6 +333,72 @@ const Landing = () => {
                 </div>
             </div>
 
+        </div>
+        <div className="block md:hidden bg-white py-10 px-5">
+            <div className="flex flex-col space-y-8">
+                <div>
+                    <div className="flex flex-row items-center justify-between mb-5">
+                        <p className="text-xl font-bold text-black">Products</p>
+                        {openProducts ? <MinusCircle color="black" size={30} onClick={openPs}/>: <PlusCircle color="black" size={30} onClick={openPs}/>}
+                    </div>
+                    {openProducts && <div className="flex flex-col space-y-2">
+                        <p className="text-gray-400 hover:text-black transition duration-300 cursor-pointer">Buy Gift Cards</p>
+                        <p className="text-gray-400 hover:text-black transition duration-300 cursor-pointer">Sell Gift Cards</p>
+                        <p className="text-gray-400 hover:text-black transition duration-300 cursor-pointer">Bill Payments</p>
+                        <p className="text-gray-400 hover:text-black transition duration-300 cursor-pointer">Currency Exchange</p>
+                        <p className="text-gray-400 hover:text-black transition duration-300 cursor-pointer">Bank Accounts</p>
+                    </div>}
+                </div>
+                <div>
+                    <div className="flex flex-row items-center justify-between mb-5">
+                        <p className="text-xl font-bold text-black">Available GiftCards</p>
+                        {openGift ? <MinusCircle color="black" size={30} onClick={opengt}/>: <PlusCircle color="black" size={30} onClick={opengt}/>}
+                    </div>
+                    {openGift && <div className="flex flex-col space-y-2">
+                        <p className="text-gray-400 hover:text-black transition duration-300 cursor-pointer">Travel Gift Cards</p>
+                        <p className="text-gray-400 hover:text-black transition duration-300 cursor-pointer">LifeStyle Gift Cards</p>
+                        <p className="text-gray-400 hover:text-black transition duration-300 cursor-pointer">Music Gift Cards</p>
+                        <p className="text-gray-400 hover:text-black transition duration-300 cursor-pointer">SuperMarket Gift Cards</p>
+                        <p className="text-gray-400 hover:text-black transition duration-300 cursor-pointer">Gaming Gift Cards</p>
+                        <p className="text-gray-400 hover:text-black transition duration-300 cursor-pointer">Streaming Gift Cards</p>
+                    </div>}
+                </div>
+                <div>
+                    <div className="flex flex-row items-center justify-between mb-5">
+                        <p className="text-xl font-bold text-black">Resources</p>
+                        {openResource ? <MinusCircle color="black" size={30} onClick={openr}/>: <PlusCircle color="black" size={30} onClick={openr}/>}
+                    </div>
+                    {openResource && <div className="flex flex-col space-y-2">
+                        <p className="text-gray-400 hover:text-black transition duration-300 cursor-pointer">Help Center</p>
+                        <p className="text-gray-400 hover:text-black transition duration-300 cursor-pointer">Blog</p>
+                        <p className="text-gray-400 hover:text-black transition duration-300 cursor-pointer">Support</p>
+                        <p className="text-gray-400 hover:text-black transition duration-300 cursor-pointer">Get In Touch</p>
+                    </div>}
+                </div>
+                <div>
+                    <div className="flex flex-row items-center justify-between mb-5">
+                        <p className="text-xl font-bold text-black">Company</p>
+                        {openCompany ? <MinusCircle color="black" size={30} onClick={opency}/>: <PlusCircle color="black" size={30} onClick={opency}/>}
+                    </div>
+                    {openCompany && <div className="flex flex-col space-y-2">
+                        <p className="text-gray-400 hover:text-black transition duration-300 cursor-pointer">Partners</p>
+                        <p className="text-gray-400 hover:text-black transition duration-300 cursor-pointer">Privacy Policy</p>
+                        <p className="text-gray-400 hover:text-black transition duration-300 cursor-pointer">Terms Of Service</p>
+                    </div>}                   
+                </div>
+                <div>
+                    <div className="flex flex-row items-center justify-between mb-5">
+                        <p className="text-xl font-bold text-black">Connect</p>
+                        {openConnect ? <MinusCircle color="black" size={30} onClick={openct}/>: <PlusCircle color="black" size={30} onClick={openct}/>}
+                    </div>
+                    {openConnect && <div className="flex flex-col space-y-2">
+                        <p className="text-gray-400 hover:text-black transition duration-300 cursor-pointer">Get In Touch</p>
+                        <p className="text-gray-400 hover:text-black transition duration-300 cursor-pointer">veexchange_support@gmail.com</p>
+                        <p className="text-gray-400 hover:text-black transition duration-300 cursor-pointer">+234 (0) 80 1 234 5678</p>
+                        <p className="text-gray-400 hover:text-black transition duration-300 cursor-pointer">+234 (0) 80 8 765 4321</p>
+                    </div>}
+                </div>
+            </div>
         </div>
 
 
